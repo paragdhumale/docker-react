1e9095709230bd9f59cc6823cc668b366c1b8525
“paragdhumale” <parag.dhumale@gmail.com>
2019-11-28 08:47:29 -0800

    added travis deploy config

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Finally changed again!!
+          Finally changed again!! Now on AWS!!
         </p>
         <a
           className="App-link"
